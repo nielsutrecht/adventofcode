1a755d6226ba23f8c94d3a1b50c28f46f1388e92
Niels Dommerholt <ndommerholt@gmail.com>
2015-12-07 16:05:07 +0100

    Add Day2 to test set

--- src/main/scala/com/nibado/projects/adventscala/AdventOfCode.scala
+++ src/main/scala/com/nibado/projects/adventscala/AdventOfCode.scala
@@ -1,8 +1,5 @@
 package com.nibado.projects.adventscala
 
-/**
-  * Created by nielsdommerholt on 07/12/15.
-  */
 object AdventOfCode {
   def main(args: Array[String]): Unit = {
     val list = List(new Day01, new Day02)
