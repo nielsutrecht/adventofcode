f78e378159ad219d1bdaf41f3e2af5269492f73d
Niels Dommerholt <nielsutrecht@gmail.com>
2015-12-11 08:07:03 +0100

    Day 11 Java solution

---
+++ src/main/java/com/nibado/projects/advent/Day11.java
@@ -0,0 +1,88 @@
+package com.nibado.projects.advent;
+
+import static com.nibado.projects.advent.Util.printAnswer;
+import static com.nibado.projects.advent.Util.readResourceAsString;
+
+public class Day11 implements Runnable {
+    private static final char[] ILLEGAL = "ilo".toCharArray();
+    @Override
+    public void run() {
+        char[] password = readResourceAsString("/day11.txt").toCharArray();
+
+        nextValid(password);
+        printAnswer(11, "One", new String(password));
+        nextValid(password);
+        printAnswer(11, "Two", new String(password));
+    }
+
+    public static void nextValid(char[] password) {
+        do {
+            increment(password);
+        }
+        while(!isValid(password));
+    }
+
+    public static void increment(char[] password) {
+        for(int i = password.length - 1;i >= 0;i--) {
+            password[i]++;
+            if(password[i] > 'z') {
+                password[i] = 'a';
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    public static boolean isValid(char[] password) {
+        if(password.length != 8) {
+            throw new IllegalArgumentException("Invalid length: " + password.length);
+        }
+
+        return hasStraight(password, 3) && !containsIllegal(password) && countPairs(password) >= 2;
+    }
+
+    public static boolean hasStraight(char[] password, int length) {
+        for(int i = 0;i < password.length - length;i++) {
+            int count = 0;
+            for(int j = 0;j < length;j++) {
+                if(password[i + j] == password[i] + (char)j) {
+                    count ++;
+                }
+            }
+            if(count == length) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    public static boolean containsIllegal(char[] password) {
+        for(int i = 0;i < password.length;i++) {
+            for(int j = 0;j < ILLEGAL.length;j++) {
+                if(password[i] == ILLEGAL[j]) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    public static int countPairs(char[] password) {
+        int count = 0;
+        for(int i = 0;i < password.length - 1;i++) {
+            if(password[i] == password[i + 1]) {
+                count++;
+                i++;
+            }
+        }
+
+        return count;
+    }
+
+    public static void main(String... argv) {
+        new Day11().run();
+    }
+}
