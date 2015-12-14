f0382acd85a9102dc00b8a16ef01382a9baa6e4a
Niels Dommerholt <ndommerholt@gmail.com>
2015-12-14 16:08:08 +0100

    Day 12 scala

--- src/main/scala/com/nibado/projects/adventscala/Day.scala
+++ src/main/scala/com/nibado/projects/adventscala/Day.scala
@@ -1,12 +1,29 @@
 package com.nibado.projects.adventscala
 
+import java.io.InputStream
+
+import spray.json.{JsObject, JsonParser}
+
 trait Day extends Runnable {
+  private def getStream(file: String): InputStream = {
+    val stream = getClass.getResourceAsStream(file)
+    if(stream == null) {
+      throw new IllegalArgumentException("Resource " + file + " not found.");
+    }
+
+    stream
+  }
+
   def getResourceAsString(file: String): String = {
-    scala.io.Source.fromInputStream(getClass.getResourceAsStream(file)).mkString
+    scala.io.Source.fromInputStream(getStream(file)).mkString
   }
 
   def getResource(file: String): List[String] = {
     scala.io.Source.fromInputStream(getClass.getResourceAsStream(file)).getLines.toList
+  }
+
+  def getResourceAsJson(file: String): JsObject = {
+    JsonParser(getResourceAsString(file)).asJsObject
   }
 
   def printAnswer(day: Int, section: String, result: Any): Unit = {
--- src/main/scala/com/nibado/projects/adventscala/Day12.scala
+++ src/main/scala/com/nibado/projects/adventscala/Day12.scala
@@ -1,5 +1,25 @@
 package com.nibado.projects.adventscala
 
+import spray.json._
+
 class Day12 extends Day {
-  override def run(): Unit = {}
+  override def run(): Unit = {
+    val json = getResourceAsJson("/day12.txt")
+
+    printAnswer(12, "One", sumTree(json, n => false))
+    printAnswer(12, "Two", sumTree(json, n => hasValue(n, "red")))
+  }
+
+  def sumTree(node: JsValue, filter: (Map[String, JsValue] => Boolean)) : Int = {
+    node match {
+      case JsNumber(n) => n.intValue()
+      case JsArray(a) => a.map(e => sumTree(e, filter)).sum
+      case JsObject(obj) => if(filter(obj)) { 0 } else obj.values.map(v => sumTree(v, filter)).sum
+      case _ => 0
+    }
+  }
+
+  def hasValue(obj: Map[String, JsValue], value: String) : Boolean = {
+    obj.values.exists { case JsString(s) => s.equals(value) case _ => false }
+  }
 }
