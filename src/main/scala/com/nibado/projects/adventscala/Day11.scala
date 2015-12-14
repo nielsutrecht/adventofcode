4ebba14562246794b27b928d43626ec402d53bc8
Niels Dommerholt <ndommerholt@gmail.com>
2015-12-14 14:51:14 +0100

    Day 11 Scala

--- src/main/scala/com/nibado/projects/adventscala/Day11.scala
+++ src/main/scala/com/nibado/projects/adventscala/Day11.scala
@@ -1,21 +1,47 @@
 package com.nibado.projects.adventscala
 
 class Day11 extends Day {
-  override def run(): Unit = {
+  val illegal = "ilo".toList
 
+  override def run(): Unit = {
+    val password = new StringBuilder(getResourceAsString("/day11.txt"))
+
+    printAnswer(11, "One", nextValid(password))
+    printAnswer(11, "Two", nextValid(password))
   }
 
-  def increment(password: Array[Char]) = {
+  def increment(password: StringBuilder): Unit = {
     (password.length - 1 to 0 by -1).foreach(i => {
-      val c = password(i) + 1.toChar
-
-
+      password.update(i, (password(i) + 1).toChar)
+      if(password(i) > 'z') {
+        password.update(i, 'a')
+      }
+      else {
+        return
+      }
     })
   }
-}
 
-object Test {
-  def main(argv: Array[String]) = {
-    new Day11().run()
+  def nextValid(password: StringBuilder): StringBuilder = {
+    do {
+      increment(password)
+    }
+    while(!threeStraight(password) || containsIllegal(password) || !hasTwoPairs(password))
+
+    password
+  }
+
+  def threeStraight(s: StringBuilder) =
+    s.toList.sliding(3).exists { case Seq(a, b, c) =>
+      a - b == -1 && b - c == -1
+    }
+
+  def containsIllegal(s: StringBuilder) =
+    s.exists(c => illegal.contains(c))
+
+  def hasTwoPairs(s: StringBuilder) = {
+    val pairs = s.sliding(2).toSeq
+
+    pairs.lastIndexWhere(l => l(0) == l(1)) - pairs.indexWhere(l => l(0) == l(1)) > 1
   }
 }
