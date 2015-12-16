08281d2eb5c29e1754978ceea4474d80d4c59651
Niels Dommerholt <ndommerholt@gmail.com>
2015-12-16 10:17:13 +0100

    Condensed down Day 16 a bit

--- src/main/java/com/nibado/projects/advent/Day16.java
+++ src/main/java/com/nibado/projects/advent/Day16.java
@@ -1,8 +1,8 @@
 package com.nibado.projects.advent;
 
+import java.util.HashMap;
 import java.util.List;
-import java.util.Objects;
-import java.util.function.Function;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static com.nibado.projects.advent.Util.printAnswer;
@@ -12,18 +12,7 @@ public class Day16 implements Runnable {
     @Override
     public void run() {
         List<Sue> sues = readResource("/day16.txt").stream().map(Sue::of).collect(Collectors.toList());
-        Sue target = new Sue();
-
-        target.children = 3;
-        target.cats = 7;
-        target.samoyeds = 2;
-        target.pomeranians = 3;
-        target.akitas = 0;
-        target.vizslas = 0;
-        target.goldfish = 5;
-        target.trees = 3;
-        target.cars = 2;
-        target.perfumes = 1;
+        Sue target = new Sue().add("children", 3).add("cats", 7).add("samoyeds", 2).add("pomeranians", 3).add("akitas", 0).add("vizslas", 0).add("goldfish", 5).add("trees", 3).add("cars", 2).add("perfumes", 1);
 
         printAnswer(16, "One", sues.stream().filter(s -> s.matches(target, 1)).findFirst().get().num);
         printAnswer(16, "Two", sues.stream().filter(s -> s.matches(target, 2)).findFirst().get().num);
@@ -35,16 +24,7 @@ public class Day16 implements Runnable {
 
     public static class Sue {
         private int num;
-        private int children = -1;
-        private int cats = -1;
-        private int samoyeds = -1;
-        private int pomeranians = -1;
-        private int akitas = -1;
-        private int vizslas = -1;
-        private int goldfish = -1;
-        private int trees = -1;
-        private int cars = -1;
-        private int perfumes = -1;
+        private final Map<String, Integer> properties = new HashMap<>();
 
         public static Sue of(String line) {
             String[] parts = line.split(":", 2);
@@ -54,51 +34,48 @@ public class Day16 implements Runnable {
 
             for(String prop: parts[1].trim().split(",")) {
                 parts = prop.split(": ");
-                int amount = Integer.parseInt(parts[1]);
 
-                switch(parts[0].trim()) {
-                    case "children": sue.children = amount;break;
-                    case "cats": sue.cats = amount;break;
-                    case "samoyeds": sue.samoyeds = amount;break;
-                    case "pomeranians": sue.pomeranians = amount;break;
-                    case "akitas": sue.akitas = amount;break;
-                    case "vizslas": sue.vizslas = amount;break;
-                    case "goldfish": sue.goldfish = amount;break;
-                    case "trees": sue.trees = amount;break;
-                    case "cars": sue.cars = amount;break;
-                    case "perfumes": sue.perfumes = amount;break;
-                    default: throw new IllegalArgumentException("Unknown property: " + parts[0].trim());
-                }
+                sue. add(parts[0].trim(), Integer.parseInt(parts[1]));
             }
 
             return sue;
         }
 
-        public boolean matches(Sue other, int part) {
-            return matches(other, s -> s.akitas) &&
-                    matches(other, s -> s.cars) &&
-                    matches(other, s -> s.cats, part == 1 ? Mode.EQ : Mode.GT) &&
-                    matches(other, s -> s.children) &&
-                    matches(other, s -> s.goldfish, part == 1 ? Mode.EQ : Mode.LT) &&
-                    matches(other, s -> s.perfumes) &&
-                    matches(other, s -> s.pomeranians, part == 1 ? Mode.EQ : Mode.LT) &&
-                    matches(other, s -> s.samoyeds) &&
-                    matches(other, s -> s.trees, part == 1 ? Mode.EQ : Mode.GT) &&
-                    matches(other, s -> s.vizslas);
+        public Sue add(String property, int value) {
+            properties.put(property, value);
+
+            return this;
         }
 
-        private boolean matches(Sue other, Function<Sue, Integer> property) {
+        public int get(String property) {
+            return properties.getOrDefault(property, -1);
+        }
+
+        public boolean matches(Sue other, int part) {
+            return matches(other, "akitas") &&
+                    matches(other, "cars") &&
+                    matches(other, "cats", part == 1 ? Mode.EQ : Mode.GT) &&
+                    matches(other, "children") &&
+                    matches(other, "goldfish", part == 1 ? Mode.EQ : Mode.LT) &&
+                    matches(other, "perfumes") &&
+                    matches(other, "pomeranians", part == 1 ? Mode.EQ : Mode.LT) &&
+                    matches(other, "samoyeds") &&
+                    matches(other, "trees", part == 1 ? Mode.EQ : Mode.GT) &&
+                    matches(other, "vizslas");
+        }
+
+        private boolean matches(Sue other, String property) {
             return matches(other, property, Mode.EQ);
         }
 
-        private boolean matches(Sue other, Function<Sue, Integer> property, Mode mode) {
-            if(property.apply(this) == -1 || property.apply(other) == -1) {
+        private boolean matches(Sue other, String property, Mode mode) {
+            if(get(property) == -1 || other.get(property) == -1) {
                 return true;
             }
             switch(mode) {
-                case EQ: return Objects.equals(property.apply(this), property.apply(other));
-                case LT: return property.apply(this) < property.apply(other);
-                case GT: return property.apply(this) > property.apply(other);
+                case EQ: return get(property) == other.get(property);
+                case LT: return get(property) < other.get(property);
+                case GT: return get(property) > other.get(property);
             }
 
             return false;
