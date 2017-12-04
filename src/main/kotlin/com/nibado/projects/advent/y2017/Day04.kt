7d584813475f99f07756b2c7df5ad82027cddf74
Niels Dommerholt <ndommerholt@gmail.com>
2017-12-04 08:27:51 +0100

    Formatting

--- src/main/kotlin/com/nibado/projects/advent/y2017/Day04.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2017/Day04.kt
@@ -7,15 +7,16 @@ import kotlin.streams.toList
 object Day04 : Day {
     override fun main(args: Array<String>) {
         val lines = resourceLines(4).map {
-            it.split(" ").toList() }.toList()
+            it.split(" ").toList()
+        }.toList()
 
-            println(part1(lines))
-            println(part2(lines))
+        println(part1(lines))
+        println(part2(lines))
     }
 
     fun part1(lines: List<List<String>>) =
-        lines.filter { it.size == HashSet<String>(it).size }.count()
+            lines.filter { it.size == HashSet<String>(it).size }.count()
 
     fun part2(lines: List<List<String>>) =
-        lines.map { it.map { it.toList().sorted().toString() } }.filter { it.size == HashSet<String>(it).size }.count()
+            lines.map { it.map { it.toList().sorted().toString() } }.filter { it.size == HashSet<String>(it).size }.count()
 }
