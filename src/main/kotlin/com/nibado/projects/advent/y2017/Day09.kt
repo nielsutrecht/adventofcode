c9d9963cf9742a51b38d2706d5d3949b761b08e1
Niels Dommerholt <ndommerholt@gmail.com>
2017-12-09 08:33:38 +0100

    Cleanup

--- src/main/kotlin/com/nibado/projects/advent/y2017/Day09.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2017/Day09.kt
@@ -5,36 +5,33 @@ import com.nibado.projects.advent.resourceString
 import java.util.*
 
 object Day09 : Day {
-    val input: Pair<String, Int> by lazy { Day09.clean((resourceString(9))) }
+    private val input: Pair<String, Int> by lazy { Day09.clean((resourceString(9))) }
 
-    fun clean(input: String) : Pair<String, Int> {
+    private fun clean(input: String): Pair<String, Int> {
         val builder = StringBuilder(input.length)
         var inGarbage = false
         var skip = false
         var count = 0
-        for(c in input) {
-            if(skip) {
+        for (c in input) {
+            if (skip) {
                 skip = false
                 continue
             }
-            if(c == '!') {
-                skip = true
-                //count++
-                continue
-            } else if(c == '<') {
-                if(inGarbage) {
-                    count++
+            when (c) {
+                '!' -> skip = true
+                '<' -> {
+                    if (inGarbage) {
+                        count++
+                    }
+                    inGarbage = true
                 }
-                inGarbage = true
-                continue
-            } else if(c == '>') {
-                inGarbage = false
-                continue
-            } else {
-                if(!inGarbage) {
-                    builder.append(c)
-                } else {
-                    count++
+                '>' -> inGarbage = false
+
+                else -> {
+                    when (inGarbage) {
+                        true -> count++
+                        false -> builder.append(c)
+                    }
                 }
             }
         }
@@ -42,16 +39,16 @@ object Day09 : Day {
         return Pair(builder.toString(), count)
     }
 
-    fun score(input: String) : Int {
+    private fun score(input: String): Int {
         val chars = LinkedList<Char>(input.toCharArray().toList())
         return score(chars, 0)
     }
 
-    fun score(input: Queue<Char>, depth: Int) : Int {
+    private fun score(input: Queue<Char>, depth: Int): Int {
         var score = depth
-        while(input.isNotEmpty()) {
+        while (input.isNotEmpty()) {
             val c = input.remove()
-            if(c == '{') {
+            if (c == '{') {
                 score += score(input, depth + 1)
             } else if (c == '}') {
                 return score
