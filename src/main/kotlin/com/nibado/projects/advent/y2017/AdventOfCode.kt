60292601ea7cd75ba628562a0a64f5900fcc2c5e
Niels Dommerholt <ndommerholt@gmail.com>
2017-12-24 09:29:07 +0100

    Day 24

--- src/main/kotlin/com/nibado/projects/advent/y2017/AdventOfCode.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2017/AdventOfCode.kt
@@ -8,7 +8,7 @@ fun main(args: Array<String>) {
             Day06, Day07, Day08, Day09, Day10,
             Day11, Day12, Day13, Day14, Day15,
             Day16, Day17, Day18, Day19, Day20,
-            Day21, Day22, Day23)
+            Day21, Day22, Day23, Day24)
 
     val day = if(args.isEmpty()) 0 else args[0].toInt()
 
---
+++ src/main/kotlin/com/nibado/projects/advent/y2017/Day24.kt
@@ -0,0 +1,35 @@
+package com.nibado.projects.advent.y2017
+
+import com.nibado.projects.advent.Day
+import com.nibado.projects.advent.resourceLines
+
+object Day24 : Day {
+    private val input = resourceLines(24).map { it.split("/").map { it.toInt() } }.map { it[0] to it[1] }.sortedBy { it.first }
+    private val solution : List<List<Pair<Int,Int>>> by lazy { solve() }
+
+    override fun part1() = solution.last().map { it.first + it.second }.sum().toString()
+    override fun part2(): String {
+        val maxLength = solution.maxBy { it.size }!!.size
+
+        return solution.last { it.size == maxLength }.map { it.first + it.second }.sum().toString()
+    }
+
+    private fun solve(): List<List<Pair<Int,Int>>> {
+        val solutions = mutableListOf<List<Pair<Int, Int>>>()
+
+        search(listOf(), 0, input.toSet(), solutions)
+
+        return solutions.map { it to it.map { it.first + it.second }.sum()}.filter { it.second > 1000 }.sortedBy { it.second }.map { it.first }
+    }
+
+    private fun search(current: List<Pair<Int, Int>>, port: Int, available: Set<Pair<Int, Int>>, solutions: MutableList<List<Pair<Int, Int>>>) {
+        solutions.add(current)
+
+        available.filter { it.first == port }.forEach {
+            search(current + it, it.second, available.minus(it), solutions)
+        }
+        available.filter { it.second == port }.forEach {
+            search(current + it, it.first, available.minus(it), solutions)
+        }
+    }
+}
