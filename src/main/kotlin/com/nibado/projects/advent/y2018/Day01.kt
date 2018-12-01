39fd6d4f38711956f6e4aa06d5c21f22376f6d52
Niels Dommerholt <ndommerholt@gmail.com>
2018-12-01 07:49:51 +0100

    Year 2018 day 1

--- src/main/kotlin/com/nibado/projects/advent/y2018/Day01.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2018/Day01.kt
@@ -1,8 +1,27 @@
 package com.nibado.projects.advent.y2018
 
 import com.nibado.projects.advent.Day
+import com.nibado.projects.advent.resourceLines
 
 object Day01 : Day {
-    override fun part1() = ""
-    override fun part2() = ""
+    private val numbers = resourceLines(2018, 1).map { it.toInt() }
+
+    override fun part1() = numbers.sum().toString()
+
+    override fun part2() : String {
+        val set = mutableSetOf<Int>()
+        var freq = 0
+
+        while(true) {
+            for (i in numbers) {
+                freq += i
+
+                if (set.contains(freq)) {
+                    return freq.toString()
+                }
+
+                set += freq
+            }
+        }
+    }
 }
