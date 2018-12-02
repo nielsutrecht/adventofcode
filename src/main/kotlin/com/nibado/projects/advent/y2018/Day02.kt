64354916ff37c214bed72c23a60aaf9038ee18c2
Niels Dommerholt <ndommerholt@gmail.com>
2018-12-02 09:24:54 +0100

    Formatting

--- src/main/kotlin/com/nibado/projects/advent/y2018/Day02.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2018/Day02.kt
@@ -3,26 +3,26 @@ package com.nibado.projects.advent.y2018
 import com.nibado.projects.advent.Day
 import com.nibado.projects.advent.resourceLines
 
-    object Day02 : Day {
-        private val ids = resourceLines(2018, 2)
+object Day02 : Day {
+    private val ids = resourceLines(2018, 2)
 
-        override fun part1(): String {
-            val maps = ids.map {
-                it.toCharArray().asSequence()
-                        .groupBy { it }
-                        .map { it.key to it.value.size }
-                        .toMap()
-            }
-
-            val countTwo = maps.count { it.entries.any { it.value == 2 } }
-            val countThree = maps.count { it.entries.any { it.value == 3 } }
-
-            return (countTwo * countThree).toString()
+    override fun part1(): String {
+        val maps = ids.map {
+            it.toCharArray().asSequence()
+                    .groupBy { it }
+                    .map { it.key to it.value.size }
+                    .toMap()
         }
 
-        override fun part2() =
-                ids.flatMap { i -> ids.map { i to it } }
-                        .map { it.first.toCharArray().intersect(it.second.toCharArray().asIterable()) }
-                        .first { it.size == ids[0].length - 1 }
-                        .joinToString(separator = "")
+        val countTwo = maps.count { it.entries.any { it.value == 2 } }
+        val countThree = maps.count { it.entries.any { it.value == 3 } }
+
+        return (countTwo * countThree).toString()
     }
+
+    override fun part2() =
+            ids.flatMap { i -> ids.map { i to it } }
+                    .map { it.first.toCharArray().intersect(it.second.toCharArray().asIterable()) }
+                    .first { it.size == ids[0].length - 1 }
+                    .joinToString(separator = "")
+}
