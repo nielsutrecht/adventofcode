e32e02062b87f9c8d300830d6772059a2737c933
Niels Dommerholt <ndommerholt@gmail.com>
2018-12-06 09:19:07 +0100

    Cleanup

--- src/main/kotlin/com/nibado/projects/advent/y2018/Day06.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2018/Day06.kt
@@ -4,33 +4,21 @@ import com.nibado.projects.advent.Day
 import com.nibado.projects.advent.Point
 import com.nibado.projects.advent.Rectangle
 import com.nibado.projects.advent.resourceLines
-import java.util.*
 
 object Day06 : Day {
     private val input = resourceLines(2018, 6).map { it.split(", ").map { it.toInt() } }.map { Point(it[0], it[1]) }
     private val rect = Rectangle(Point(input.minBy { it.x }!!.x - 1, input.minBy { it.y }!!.y - 1), Point(input.maxBy { it.x }!!.x + 1, input.maxBy { it.y }!!.y + 1))
 
-    override fun part1(): Int {
-        val nearest = rect.points().map { p -> p to nearest(p) }
+    override fun part1() = rect.points()
+            .map { p -> p to nearest(p) }
+            .filterNot { rect.onEdge(it.first) || it.second == null }
+            .groupBy { it.second }
+            .map { it.key!! to it.value.size }
+            .maxBy { it.second }!!.second
 
-        val grouped = nearest.filterNot { it.second == null }.groupBy { it.second!! }
+    override fun part2() = rect.points().count { distanceSum(it) < 10000 }
 
-        val grouped2 = grouped.filterNot { it.value.map { it.first }.any { rect.onEdge(it) } }.maxBy { it.value.size }!!
-
-        return grouped2.value.size
-    }
-
-    override fun part2(): Int {
-        val areaSet = rect.points().map { p -> p to distanceSum(p) }.filter { it.second < 10000 }.map { it.first }.toSet()
-
-        val result = floodFill(areaSet).sortedBy { it.size }
-
-        result.forEachIndexed { index, set -> println("$index: ${set.size}") }
-
-        return result.first().size
-    }
-
-    private fun distanceSum(p : Point) : Int {
+    private fun distanceSum(p: Point): Int {
         return input.map { i -> i to i.manhattan(p) }.sumBy { it.second }
     }
 
@@ -43,29 +31,5 @@ object Day06 : Day {
         } else {
             a.first
         }
-    }
-
-    private fun floodFill(set: Set<Point>) : List<Set<Point>> {
-        val visited = mutableSetOf<Point>()
-        val result : MutableList<Set<Point>> = mutableListOf()
-
-        while(set.size > visited.size) {
-            var area = mutableSetOf<Point>()
-
-            result += area
-
-            val stack = Stack<Point>()
-            stack += set.filterNot { visited.contains(it) }.first()
-
-            while(!stack.empty()) {
-                val current = stack.pop()
-                area.add(current)
-                visited.add(current)
-
-                stack += current.neighbors().filter { set.contains(it) && !visited.contains(it) }
-            }
-        }
-
-        return result
     }
 }
