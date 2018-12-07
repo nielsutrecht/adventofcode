87f60a6e680211f2ce9b69bc508d66cd0193df89
Niels Dommerholt <ndommerholt@gmail.com>
2018-12-07 08:27:49 +0100

    Refactoring

--- src/main/kotlin/com/nibado/projects/advent/y2018/Day07.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2018/Day07.kt
@@ -7,58 +7,40 @@ object Day07 : Day {
     private val regex = "Step ([A-Z]) must be finished before step ([A-Z]) can begin\\.".toRegex()
     private val input = resourceRegex(2018, 7, regex).map { it.drop(1) }.map { (a, b) -> a.first() to b.first() }
     private val characters = input.flatMap { listOf(it.first, it.second) }.toSet().sorted().toList()
-    private val prereqs : Map<Char, Set<Char>> by lazy {
-        val prereqs = mutableMapOf<Char, MutableSet<Char>>()
-
-        input.forEach {
-            prereqs.computeIfAbsent(it.second) { mutableSetOf() }.add(it.first)
-        }
-
-        prereqs
-    }
+    private val preReqs = input.groupBy { it.second }.map { it.key to it.value.map { it.first }.toSet() }.toMap()
 
     override fun part1(): String {
-        val completed = mutableSetOf<Char>()
         val result = mutableListOf<Char>()
 
-        while (completed.size < characters.size) {
-            val canComplete = characters.filterNot { completed.contains(it) }
-                    .filter { a -> !prereqs.containsKey(a) || prereqs[a]!!.all { b -> completed.contains(b) } }
-
-            val c = canComplete.first()
-
-            completed += c
-            result += c
+        while (result.size < characters.size) {
+            result += characters
+                    .filterNot { result.contains(it) }
+                    .first { a -> !preReqs.containsKey(a) || preReqs[a]!!.all { b -> result.contains(b) } }
         }
 
         return result.joinToString("")
     }
 
     override fun part2(): Int {
-        val completed = mutableSetOf<Char>()
         val result = mutableListOf<Char>()
 
         var workers = 0
         var second = 0
         var until = mutableMapOf<Char, Int>()
 
-        while (completed.size < characters.size) {
-            val done = until.filter { it.value == second }.keys.sorted()
+        while (result.size < characters.size) {
+            with(until.filter { it.value == second }.keys.sorted()) {
+                forEach { until.remove(it) }
+                workers -= size
+                result += this
+            }
 
-            done.forEach { until.remove(it) }
-
-            workers -= done.size
-
-            completed += done
-            result += done
-
-            val canComplete = characters.filterNot { completed.contains(it) || until.keys.contains(it) }
-                    .filter { a -> !prereqs.containsKey(a) || prereqs[a]!!.all { b -> completed.contains(b) } }
+            characters.filterNot { result.contains(it) || until.keys.contains(it) }
+                    .filter { a -> !preReqs.containsKey(a) || preReqs[a]!!.all { b -> result.contains(b) } }
                     .sorted()
                     .take(5 - workers)
-
-            workers += canComplete.size
-            canComplete.forEach { until[it] = second + (it - 'A' + 61) }
+                    .also { workers += it.size }
+                    .forEach { until[it] = second + (it - 'A' + 61) }
 
             second++
         }
