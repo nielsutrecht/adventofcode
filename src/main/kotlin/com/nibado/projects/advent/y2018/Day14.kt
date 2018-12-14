00ae65c98539ceedc5d2742318c90badb974ccd9
Niels Dommerholt <ndommerholt@gmail.com>
2018-12-14 12:15:22 +0100

    Refactor

--- src/main/kotlin/com/nibado/projects/advent/y2018/Day14.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2018/Day14.kt
@@ -4,38 +4,39 @@ import com.nibado.projects.advent.Day
 
 object Day14 : Day {
     private const val PUZZLE_INPUT = 330121
-    private val start = "37".map { it - '0' }
+    private const val start = "37"
 
     override fun part1(): String {
         val elves = mutableListOf(0, 1)
-        val recipes = start.toMutableList()
+        val recipes = StringBuilder(start)
 
-        while (recipes.size < PUZZLE_INPUT + 10) {
+        while (recipes.length < PUZZLE_INPUT + 10) {
             recipes(recipes, elves)
         }
 
-        return recipes.drop(PUZZLE_INPUT).take(10).joinToString("")
+        return recipes.substring(PUZZLE_INPUT, PUZZLE_INPUT + 10)
     }
 
     override fun part2(): Int {
         val num = PUZZLE_INPUT.toString()
         val elves = mutableListOf(0, 1)
-        val recipes = start.toMutableList()
+        val recipes = StringBuilder(30000000)
+        recipes.append(start)
 
         while(true) {
             recipes(recipes, elves)
 
-            val lastBit = if(recipes.size > 10) recipes.subList(recipes.size - 10, recipes.size).joinToString("") else ""
+            val lastBit = if(recipes.length > 10) recipes.subSequence(recipes.length - 10, recipes.length) else ""
 
             if(lastBit.contains(num)) {
-                return(recipes.size - 10 + lastBit.indexOf(num) )
+                return(recipes.length - 10 + lastBit.indexOf(num) )
             }
         }
     }
 
-    private fun recipes(recipes: MutableList<Int>, elves: MutableList<Int>) {
-        recipes += elves.indices.map { recipes[elves[it]] }.sum().toString().map { it - '0' }
-        elves[0] = ((elves[0] + recipes[elves[0]] + 1) % recipes.size)
-        elves[1] = ((elves[1] + recipes[elves[1]] + 1) % recipes.size)
+    private fun recipes(recipes: StringBuilder, elves: MutableList<Int>) {
+        recipes.append(elves.indices.map { recipes[elves[it]] - '0' }.sum())
+        elves[0] = ((elves[0] + (recipes[elves[0]] - '0') + 1) % recipes.length)
+        elves[1] = ((elves[1] + (recipes[elves[1]] - '0') + 1) % recipes.length)
     }
 }
