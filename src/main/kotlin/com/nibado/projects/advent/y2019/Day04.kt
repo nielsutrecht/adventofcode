e316791d917142e721e225ed40e3b16e5c87c23b
Niels Dommerholt <ndommerholt@bol.com>
2019-12-04 13:02:34 +0100

    Refactored to more functional approach

--- src/main/kotlin/com/nibado/projects/advent/y2019/Day04.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2019/Day04.kt
@@ -7,26 +7,8 @@ object Day04 : Day {
 
     private fun doesNotDecrease(password: List<Int>) = (0..4).none { password[it] > password[it + 1] }
     private fun hasSameTwo(password: List<Int>) = (0..4).any { password[it] == password[it + 1] }
-    private fun hasGroupOfTwo(password: List<Int>) = groups(password).contains(2)
-
-    private fun groups(digits: List<Int>) : Set<Int> {
-        var current = digits.first()
-        var count = 1
-        val counts = mutableListOf<Int>()
-        for(i in (1 until digits.size)) {
-            if(current == digits[i]) {
-                count++
-            } else {
-                counts += count
-                count = 1
-            }
-            current = digits[i]
-        }
-        counts += count
-
-        return counts.toSet()
-    }
+    private fun hasGroupOfTwoB(password: List<Int>) = ('0'.toInt() .. '9'.toInt()).any { password.lastIndexOf(it) - password.indexOf(it) == 1 }
 
     override fun part1() = range.filter(::doesNotDecrease).filter(::hasSameTwo).count()
-    override fun part2() = range.filter(::doesNotDecrease).filter(::hasGroupOfTwo).count()
+    override fun part2() = range.filter(::doesNotDecrease).filter(::hasGroupOfTwoB).count()
 }
