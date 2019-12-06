692c742f3865c18f8752d0ed8e019444488bb3bb
Niels Dommerholt <ndommerholt@bol.com>
2019-12-06 10:20:11 +0100

    Refactored Day 6

--- src/main/kotlin/com/nibado/projects/advent/y2019/Day06.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2019/Day06.kt
@@ -6,23 +6,12 @@ import com.nibado.projects.advent.resourceLines
 object Day06 : Day {
     private val orbitMap = resourceLines(2019, 6).map { it.split(")").let { (a, b) -> b to a } }.toMap()
 
-    private fun walk(current: String) : Int =
-        if(current == "COM") {
-            0
-        } else {
-            1 + walk(orbitMap.getValue(current))
-        }
-
     private fun path(current: String) : List<String> =
-        if(current == "COM") {
-            listOf("COM")
-        } else {
-            path(orbitMap.getValue(current)) + current
-        }
+        if(current == "COM") listOf("COM") else path(orbitMap.getValue(current)) + current
 
-    override fun part1() = orbitMap.keys.map { walk(it) }.sum()
+    override fun part1() = orbitMap.keys.map { path(it).size - 1 }.sum()
 
-    override fun part2() : Any {
+    override fun part2() : Int {
         val you = path("YOU").toMutableList()
         val santa  = path("SAN").toMutableList()
 
