7ebb0be0637640dcfc29fbd3e1351492ae6b9af8
Niels Dommerholt <ndommerholt@bol.com>
2019-12-09 09:56:59 +0100

    Cleanup

--- src/main/kotlin/com/nibado/projects/advent/y2019/Day09.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2019/Day09.kt
@@ -6,6 +6,6 @@ import com.nibado.projects.advent.resourceString
 object Day09 : Day {
     private val program = resourceString(2019, 9).split(",").map { it.toLong() }
 
-    override fun part1() =IntCode(program, listOf(1)).also { it.run() }.output.last()!!
+    override fun part1() = IntCode(program, listOf(1)).also { it.run() }.output.last()!!
     override fun part2() = IntCode(program, listOf(2)).also { it.run() }.output.last()!!
 }
