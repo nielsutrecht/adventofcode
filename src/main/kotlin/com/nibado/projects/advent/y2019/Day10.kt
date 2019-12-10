94c4f93ac382c164785e00389ecba563ea4edc8d
Niels Dommerholt <ndommerholt@bol.com>
2019-12-10 13:34:38 +0100

    Day 10

--- src/main/kotlin/com/nibado/projects/advent/y2019/Day10.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2019/Day10.kt
@@ -1,82 +1,53 @@
 package com.nibado.projects.advent.y2019
 
-import com.nibado.projects.advent.*
+import com.nibado.projects.advent.Day
+import com.nibado.projects.advent.Point
+import com.nibado.projects.advent.resourceLines
+import com.nibado.projects.advent.toPoint
+import kotlin.math.PI
 
 object Day10 : Day {
-    val test = """
-......#.#.
-#..#.#....
-..#######.
-.#.#.###..
-.#..#.....
-..#....#.#
-#..#....#.
-.##.#..###
-##...#..#.
-.#....####
-    """.trimIndent()
-
     private val map = resourceLines(2019, 10).map { line -> line.toCharArray().toList() }
-    private val map2 = test.split("\n").map { line -> line.toCharArray().toList() }
 
-    override fun part1() = best(toPoints(map2))
-    override fun part2() = TODO()
-
-    fun best(points: Collection<Point>): Pair<Point, Int> {
-        return points.map { it to visible(it, points) }.maxBy { it.second }!!
-    }
-
-    fun toPoints(map: List<List<Char>>): Set<Point> {
-        return map.indices.flatMap { y -> map[y].indices.map { x -> x to y } }
+    private val points: Set<Point> by lazy {
+        map.indices.flatMap { y -> map[y].indices.map { x -> x to y } }
             .filter { (x, y) -> map[y][x] == '#' }
             .map { it.toPoint() }
             .toSet()
     }
 
-    fun visible(p: Point, points: Collection<Point>): Int {
-        var counter = 0
-        var visited = 0
-        for (other in points.filterNot { it == p }) {
-            val line = Line(p, other)
-            val distanceToOther = p.distance(other)
-            val angleToOther = p.angle(other)
+    private val laser: Pair<Point, Int> by lazy {
+        points.map { it to visible(it, points) }.maxBy { it.second }!!
+    }
 
-            val onLine = points.filterNot { it == p || it == other }
-                .filter { line.distanceTo(it) <= 0.001 }
+    private fun visible(p: Point, points: Set<Point>): Int = points
+        .filterNot { it == p }
+        .map { it.angle(p) }
+        .distinct()
+        .size
 
-            val closer = onLine.filter { p.distance(it) < distanceToOther }
-            val angled = closer.filter { p.angle(it) == angleToOther }
-            val blocked = angled.isNotEmpty()
+    override fun part1() = laser.second
 
-            val distances = points.filterNot { it == p || it == other }.filterNot { p.distance(it) > distanceToOther }
-                .map { third -> third to line.distanceTo(third) }
+    override fun part2(): Int {
+        val rads = points.filterNot { it == laser.first }
+            .groupBy {laser.first.angle(it) + PI }
+            .map { e -> e.key to e.value.sortedBy { ast -> laser.first.distance(ast) }.toMutableList() }
+            .sortedBy { it.first }
+            .reversed()
 
+        var count = 0
 
-            if (!blocked) {
-                counter++
+        while (rads.any { it.second.isNotEmpty() }) {
+            val list = rads[count++ % rads.size].second
+
+            if (list.isNotEmpty()) {
+                val p = list.removeAt(0)
+                if (count == 200) {
+                    return p.x * 100 + p.y
+                }
             }
-            visited++
         }
 
-        return counter
+        throw RuntimeException()
     }
-
-
-    fun test() {
-        //val points = listOf(Point(0, 0), Point(3, 1), Point(6, 2), Point(2,1))
-
-        val points = toPoints(map2)
-        println(visible(points.first(), points))
-    }
-
-
-    fun print(map: List<List<Char>>) {
-        println(map.map { it.joinToString("") }.joinToString("\n"))
-    }
-
-
-}
-
-fun main() {
-    Day10.test()
 }
--- src/test/kotlin/com/nibado/projects/advent/y2019/Year2019Test.kt
+++ src/test/kotlin/com/nibado/projects/advent/y2019/Year2019Test.kt
@@ -6,19 +6,20 @@ import com.nibado.projects.advent.YearTest
 class Year2019Test : YearTest(input()) {
     companion object {
         fun input() = listOf(
-                TestInput(Day01, 3173518, 4757427),
-                TestInput(Day02, 2890696, 8226),
-                TestInput(Day03, 316, 16368),
-                TestInput(Day04, 2779, 1972),
-                TestInput(Day05, 9219874L, 5893654L),
-                TestInput(Day06, 117672, 277),
-                TestInput(Day07, 87138L, 17279674L),
-                TestInput(Day08, 1330, DAY08_P2),
-                TestInput(Day09, 3906448201L, 59785L)
+            TestInput(Day01, 3173518, 4757427),
+            TestInput(Day02, 2890696, 8226),
+            TestInput(Day03, 316, 16368),
+            TestInput(Day04, 2779, 1972),
+            TestInput(Day05, 9219874L, 5893654L),
+            TestInput(Day06, 117672, 277),
+            TestInput(Day07, 87138L, 17279674L),
+            TestInput(Day08, 1330, DAY08_P2),
+            TestInput(Day09, 3906448201L, 59785L),
+            TestInput(Day10, 286, 504)
         ).drop(0)
 
         const val DAY08_P2 =
-"""XXXX  XX  X  X XXXX XXXX 
+            """XXXX  XX  X  X XXXX XXXX 
 X    X  X X  X X    X    
 XXX  X  X XXXX XXX  XXX  
 X    XXXX X  X X    X    
