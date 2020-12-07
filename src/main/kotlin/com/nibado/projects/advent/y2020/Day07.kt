add11f0b91a2ad40715ca362c111ed9d3e2dac4f
Niels Dommerholt <ndommerholt@gmail.com>
2020-12-07 08:55:35 +0100

    2020 day 7 refactor

--- src/main/kotlin/com/nibado/projects/advent/y2020/Day07.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2020/Day07.kt
@@ -1,41 +1,23 @@
 package com.nibado.projects.advent.y2020
 
-import com.nibado.projects.advent.Day
-import com.nibado.projects.advent.resourceRegex
+import com.nibado.projects.advent.*
 
 object Day07 : Day {
     private val REGEX_LINES = "([a-z ]+) bags contain ([0-9a-z, ]+)\\.".toRegex()
     private val REGEX_BAG = "([0-9]+) ([a-z ]+) (bag|bags)".toRegex()
     private val bags = resourceRegex(2020, 7, REGEX_LINES).map { (_, a, b) -> parseBag(a, b) }
 
-    override fun part1() : Int {
-        val consider = mutableListOf<Bag>()
-        val found = mutableSetOf<Bag>()
+    override fun part1() = recurseUp("shiny gold").size
+    override fun part2() : Int = recurseDown("shiny gold") - 1
 
-        consider += search("shiny gold")
-        found += consider
+    private fun recurseUp(color: String) : List<Bag> = bags.filter { bag -> bag.bags.any { it.second.color == color} }
+            .let { list -> list + list.flatMap { recurseUp(it.color) } }.distinct()
 
-        while(consider.isNotEmpty()) {
-            val new = consider.flatMap { search(it.color) }
-            consider.clear()
-            consider += new.filterNot { found.contains(it) }
-            found += new
-        }
-
-        return found.size
-    }
-
-    override fun part2() : Int = count("shiny gold") - 1
-
-    private fun count(color: String) : Int = 1 + bags.first { it.color == color }.bags
-            .map { (c, bag) -> c * count(bag.color) }.sum()
-
-    private fun search(color: String) : List<Bag> = bags.filter { it.bags.any { it.second.color == color } }
+    private fun recurseDown(color: String) : Int = 1 + bags.first { it.color == color }.bags
+            .map { (c, bag) -> c * recurseDown(bag.color) }.sum()
 
     private fun parseBag(first: String, second: String) : Bag {
-        val list = if(second == "no other bags") {
-            emptyList()
-        } else {
+        val list = if(second == "no other bags") { emptyList() } else {
             second.split(", ").map { REGEX_BAG.matchEntire(it)!!.groupValues
                     .let { (_, a, b) -> a.toInt() to Bag(b, emptyList()) } }
         }
