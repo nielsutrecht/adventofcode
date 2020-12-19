8e231a5d0d5e494032c9d33fcbe5dfcfca5c9371
Niels Dommerholt <ndommerholt@gmail.com>
2020-12-19 09:05:31 +0100

    Day 19

--- src/test/kotlin/com/nibado/projects/advent/y2020/Year2020Test.kt
+++ src/test/kotlin/com/nibado/projects/advent/y2020/Year2020Test.kt
@@ -23,7 +23,8 @@ class Year2020Test : YearTest(input()) {
                 TestInput(Day15, 1025, 129262),
                 TestInput(Day16, 30869, 4381476149273),
                 TestInput(Day17, 263, 1680),
-                TestInput(Day17, 6923486965641, 70722650566361)
+                TestInput(Day18, 6923486965641, 70722650566361),
+                TestInput(Day19, 180, 323)
         ).drop(0)
     }
 }
