5cd9400670dc5602efa1a401c7f07c39a461f68a
Niels Dommerholt <nielsutrecht@gmail.com>
2021-12-01 08:45:05 +0100

    Count instead of sum

--- src/main/kotlin/com/nibado/projects/advent/y2021/Day01.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2021/Day01.kt
@@ -6,9 +6,9 @@ import com.nibado.projects.advent.resourceLines
 object Day01 : Day {
     private val values = resourceLines(2021, 1).map { it.toInt() }
 
-    override fun part1() = values.windowed(2).sumOf { (a, b) -> if (a < b) 1L else 0L }
+    override fun part1() = values.windowed(2).count { (a, b) -> a < b}
 
     override fun part2() = values
         .windowed(3).map { it.sum() }
-        .windowed(2).sumOf { (a, b) -> if (a < b) 1L else 0L }
+        .windowed(2).count { (a, b) -> a < b }
 }
--- src/test/kotlin/com/nibado/projects/advent/y2021/Year2021Test.kt
+++ src/test/kotlin/com/nibado/projects/advent/y2021/Year2021Test.kt
@@ -6,7 +6,7 @@ import com.nibado.projects.advent.YearTest
 class Year2021Test : YearTest(input()) {
     companion object {
         fun input() = listOf(
-                TestInput(Day01, 1624L, 1653L),
+                TestInput(Day01, 1624, 1653),
         ).drop(0)
     }
 }
