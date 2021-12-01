5159cae9bc56ae5229bcb29a0170bfb12768be74
Niels Dommerholt <nielsutrecht@gmail.com>
2021-12-01 08:41:42 +0100

    Cleanup again

--- src/main/kotlin/com/nibado/projects/advent/y2021/Day01.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2021/Day01.kt
@@ -3,12 +3,12 @@ package com.nibado.projects.advent.y2021
 import com.nibado.projects.advent.Day
 import com.nibado.projects.advent.resourceLines
 
-    object Day01 : Day {
-        private val values = resourceLines(2021, 1).map { it.toInt() }
+object Day01 : Day {
+    private val values = resourceLines(2021, 1).map { it.toInt() }
 
-        override fun part1() = values.windowed(2).sumOf { (a, b) -> if (a < b) 1L else 0L }
+    override fun part1() = values.windowed(2).sumOf { (a, b) -> if (a < b) 1L else 0L }
 
-        override fun part2() = values
-            .windowed(3).map { it.sum() }
-            .windowed(2).sumOf { (a, b) -> if (a < b) 1L else 0L }
-    }
+    override fun part2() = values
+        .windowed(3).map { it.sum() }
+        .windowed(2).sumOf { (a, b) -> if (a < b) 1L else 0L }
+}
