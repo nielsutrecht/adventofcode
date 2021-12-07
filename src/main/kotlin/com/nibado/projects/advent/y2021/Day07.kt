623d349f13e796b989219f364ef744309da80240
Niels Dommerholt <nielsutrecht@gmail.com>
2021-12-07 07:26:36 +0100

    Day 7

---
+++ src/main/kotlin/com/nibado/projects/advent/y2021/Day07.kt
@@ -0,0 +1,16 @@
+package com.nibado.projects.advent.y2021
+
+import com.nibado.projects.advent.*
+import kotlin.math.absoluteValue
+
+object Day07 : Day {
+    private val values = resourceString(2021, 7).split(',').map { it.toInt() }
+
+    override fun part1() = (values.minOrNull()!! .. values.maxOrNull()!!).map { pos ->
+        pos to values.map { (it - pos).absoluteValue }.sum()
+    }.minByOrNull { it.second }!!.second
+
+    override fun part2() = (values.minOrNull()!! .. values.maxOrNull()!!).map { pos ->
+        pos to values.map { (0 .. (it - pos).absoluteValue).sum() }.sum()
+    }.minByOrNull { it.second }!!.second
+}
