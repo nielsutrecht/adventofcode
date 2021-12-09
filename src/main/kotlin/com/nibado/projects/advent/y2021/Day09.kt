e9aff5aad43742ac11657324e38b90f7da34e05a
Niels Dommerholt <nielsutrecht@gmail.com>
2021-12-09 11:38:38 +0100

    Simplify Day 9

--- src/main/kotlin/com/nibado/projects/advent/y2021/Day09.kt
+++ src/main/kotlin/com/nibado/projects/advent/y2021/Day09.kt
@@ -1,6 +1,7 @@
 package com.nibado.projects.advent.y2021
 
 import com.nibado.projects.advent.*
+import java.util.*
 
 object Day09 : Day {
     private val values = resourceLines(2021, 9).map { it.toCharArray().map { it.digitToInt() } }
@@ -9,26 +10,24 @@ object Day09 : Day {
     override fun part1() = points.map { it to values[it.y][it.x] }.filter {
         val neighbors = it.first.neighborsHv().filter { it.inBound(values[0].size - 1, values.size - 1) }
         neighbors.all { n -> values[n.y][n.x] > it.second }
-    }.map { it.second + 1 }.sum()
+    }.sumOf { it.second + 1 }
 
     override fun part2() : Int {
-        val consider = points.filterNot { (x, y) -> values[y][x] == 9 }.toSet().toMutableSet()
-        val areas = mutableListOf<Set<Point>>()
+        val consider = points.filterNot { (x, y) -> values[y][x] == 9 }.toMutableSet()
+        val sizes = TreeSet<Int>()
 
         while(consider.isNotEmpty()) {
-            val area = mutableSetOf<Point>()
-            val frontier = mutableSetOf(consider.first())
+            var area = 0
+            var frontier = setOf(consider.first())
 
             while(frontier.isNotEmpty()) {
-                area += frontier
+                area += frontier.size
                 consider -= frontier
-                val newFrontier = frontier.flatMap { it.neighborsHv() }.filter { consider.contains(it) }
-                frontier.clear()
-                frontier += newFrontier
+                frontier = frontier.flatMap { it.neighborsHv() }.filter { consider.contains(it) }.toSet()
             }
-            areas += area
+            sizes += area
         }
 
-        return areas.map { it.size }.sortedDescending().take(3).let { (a,b,c) -> a * b * c }
+        return sizes.toList().takeLast(3).let { (a,b,c) -> a * b * c }
     }
 }
